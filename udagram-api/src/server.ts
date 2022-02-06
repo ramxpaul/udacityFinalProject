fc4daaf20cd4b7843a062c8690ce8d39b455d226
ramxpaul <ramxpaul@gmail.com>
2022-02-06 18:29:28 +0200

    'udagram'

--- udagram-api/src/server.ts
+++ udagram-api/src/server.ts
@@ -23,7 +23,6 @@ import { V0_FEED_MODELS, V0_USER_MODELS } from "./controllers/v0/model.index";
   app.use(bodyParser.json());
 
   app.use(cors());
-
   app.use("/api/v0/", IndexRouter);
 
   // Root URI call
