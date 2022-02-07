4a1e5e032e7116e0a11409ace383e031fb830772
ramxpaul <ramxpaul@gmail.com>
2022-02-07 20:42:38 +0200

    'udagram'

--- udagram-api/src/server.ts
+++ udagram-api/src/server.ts
@@ -19,7 +19,7 @@ import { V0_FEED_MODELS, V0_USER_MODELS } from "./controllers/v0/model.index";
 
   const app = express();
   const port = process.env.PORT;
-
+  console.log(port)
   app.use(bodyParser.json());
 
   app.use(cors());
