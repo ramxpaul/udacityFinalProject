f7873baf7c4fa6ad3aa7345b9c022b0081567235
ramxpaul <ramxpaul@gmail.com>
2022-02-07 23:15:58 +0200

    'udagram'

--- udagram-api/src/server.ts
+++ udagram-api/src/server.ts
@@ -32,7 +32,7 @@ import { V0_FEED_MODELS, V0_USER_MODELS } from "./controllers/v0/model.index";
 
   // Start the Server
   app.listen(port, () => {
-    console.log(`server running ${process.env.URL}` + ':' + `${process.env.PORT}`);
+    console.log(`server running ${process.env.URL}` + ':' + `${port}`);
     console.log(`press CTRL+C to stop server`);
   });
 })();
