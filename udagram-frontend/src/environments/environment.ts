dcb716ef9b620d9000fbb6793bef3b06f72a12d9
ramxpaul <ramxpaul@gmail.com>
2022-02-08 21:51:15 +0200

    'udagram'

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:3000/api/v0'
+  apiHost: 'udagram-api-dev2.eba-pptjhzat.us-east-1.elasticbeanstalk.com /api/v0'
 };
 
 /*
