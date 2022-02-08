27a73ed24211f039873082ce9a1bfacd81aca31c
ramxpaul <ramxpaul@gmail.com>
2022-02-08 19:50:22 +0200

    'udagram'

--- udagram-api/src/server.ts
+++ udagram-api/src/server.ts
@@ -27,7 +27,7 @@ import { V0_FEED_MODELS, V0_USER_MODELS } from "./controllers/v0/model.index";
 
   // Root URI call
   app.get("/", async (req, res) => {
-    res.send("Hello World...");
+    res.send("Hello World....");
   });
 
   // Start the Server
